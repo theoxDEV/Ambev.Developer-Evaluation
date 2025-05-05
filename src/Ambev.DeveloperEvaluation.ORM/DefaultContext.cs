9ef88d314a03ec820470d961482a53ef86a37f88
Matheus Nascimento <56891192+theoxDEV@users.noreply.github.com>
2025-05-05 10:46:10 -0300

    Add code core

--- src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -1,4 +1,5 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.ORM.Seeds;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
@@ -8,16 +9,32 @@ namespace Ambev.DeveloperEvaluation.ORM;
 
 public class DefaultContext : DbContext
 {
+    /// <summary>
+    /// Represents the Users table in the database.
+    /// </summary>
     public DbSet<User> Users { get; set; }
+
+    /// <summary>
+    /// Represents the Sales table in the database.
+    /// </summary>
+    public DbSet<Sale> Sales { get; set; }
+
+    /// <summary>
+    /// Represents the SaleItems table in the database.
+    /// </summary>
+    public DbSet<SaleItem> SaleItems { get; set; }
 
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
     {
     }
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(modelBuilder);
+        modelBuilder.Seed();
+        modelBuilder.SeedBusinessRuleExamples();
     }
 }
 public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
@@ -33,9 +50,10 @@ public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         builder.UseNpgsql(
-               connectionString,
-               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+            connectionString,
+            b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
         );
+
 
         return new DefaultContext(builder.Options);
     }
